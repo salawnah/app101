fad15f7b7f7cfca02d812e5271eb3efa8724ce6e
salawnah <sameer@alawnah.com>
2025-02-27 14:47:10 +0400

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 8080
 
-app.get('/', (req, res) => {
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/', cors(corsOptions), function (req, res, next) {
     res.send('Hello World!')
 })
 
