b693fc3e64416fe897e800cc032dc25c8a2fd7d3
salawnah <sameer@alawnah.com>
2025-03-13 14:50:25 +0400

    forth commit

--- app.js
+++ app.js
@@ -7,9 +7,23 @@ var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.get('/', cors(corsOptions), function (req, res, next) {
-    res.send('Hello World!')
+app.post('/', function (req, res, next) {
+    console.log(req.body);
+    let unit = req.body.unit;
+    let value = req.body.value;
+    var result;
+    let to_unit = unit==='C'?'F':'C';
+    if(unit==='C'){ result = value*9/5.0+32;}
+    else {result = ((value-32)*5/9.0)}
+    res.json({
+        from_unit: unit,
+        to_unit: to_unit,
+        input: value,
+        result:result
+    })
 })
 
 app.listen(port, () => {
